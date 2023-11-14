a5dbe2e92ac650c6fd1e2002744871a86484e845
desync <blurskye@proton.me>
2023-11-14 14:56:49 +0600

    added template practice folders

--- demo/script.js
+++ demo/script.js
@@ -1,25 +1,5 @@
-
-// Problem 1
-let str = "Har\""
-console.log(str.length)
-
-// Problem 2
-const sentence = 'The quick brown fox jumps over the lazy dog.';
-const word = 'fox2';
-console.log(sentence.includes(word))
-console.log(`The word "${word}" ${sentence.includes(word) ? 'is' : 'is not'} in the sentence`);
-
-// Problem 3
-console.log(word.toLowerCase())
-console.log(word.toUpperCase())
-
-// Problem 4
-let str2 = "Please give Rs 1000"
-let amount = Number.parseInt(str2.slice(15))
-console.log(amount)
-console.log(typeof amount)
-
-// Problem 5
-let friend = "Deepika"
-friend[3] = "R"
-console.log(friend) // friend is not changed, because string is immutable
+strx = [1, 2, 3, 4, 5, 6, 7, 8]
+for (let i = 0; i <= strx.length - 1; i++) {
+    console.log(strx[i]);
+}
+console.log(strx.toString())
